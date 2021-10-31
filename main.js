5ac4fb23a604e9e99c2163995525ba11d9065806
JoseTovar510 <tovarjose1013@gmail.com>
2021-10-31 13:15:12 -0700

    Fixed buttons on slider

---
+++ main.js
@@ -0,0 +1,10 @@
+//const Slider = () => {
+//    const [slideIndex, setSlideIndex] = useState(0);
+//    const handleClick = (direction) => {
+//      if (direction === "left") {
+//        setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
+//      } else {
+//        setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
+//      }
+//    };
+//}

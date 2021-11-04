2636332431c8c44d7661d783225fc83f022d7144
JoseTovar510 <tovarjose1013@gmail.com>
2021-11-04 15:46:06 -0700

    Working on button event listeners

--- main.js
+++ main.js
@@ -8,3 +8,45 @@
 //      }
 //    };
 //}
+
+//const image = document.querySelector(".image");
+//const allImages = document.querySelectorAll('.image img');
+//
+//const prevBtn = document.querySelector(".arrow");
+//const nextBtn = document.querySelector(".arrow2");
+//
+//let counter = 1;
+//const size = allImages[0].clientWidth;
+//
+//image.style.transform = 'translatex(' + (-size * counter) + 'px)';
+//
+//
+////Button Listener
+//
+//nextBtn.addEventListener('click', ()=>{
+//    image.style.transition = "transform 0.4s ease-in-out";
+//    counter++;
+//    image.style.transform = 'translatex(' + (-size * counter) + 'px)';
+//
+//});
+//
+//prevBtn.addEventListener('click', ()=>{
+//    image.style.transition = "transform 0.4s ease-in-out";
+//    counter--;
+//    image.style.transform = 'translatex(' + (-size * counter) + 'px)';
+//
+//});
+//
+//image.addEventListener('transitionend', ()=> {
+//    if(allImages[counter].id === 'lastClone') {
+//    image.style.transition = "none";
+//    counter = allImages.length -2;
+//    image.style.transform = 'translatex(' + (-size * counter) + 'px)';
+//    }
+//    if(allImages[counter].id === 'firstClone') {
+//        image.style.transition = "none";
+//        counter = allImages.length - counter;
+//        image.style.transform = 'translatex(' + (-size * counter) + 'px)';
+//    
+//        }
+//})
